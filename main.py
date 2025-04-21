9b5817fcdb124746f2db929501b9a8e75800c75a
WaltBow <walter.bowyer@ulead.ac.cr>
2025-04-21 07:53:03 -0600

    API GET para obtener usuario

--- main.py
+++ main.py
@@ -14,3 +14,10 @@ class Usuario(BaseModel):
 def guardar_usuario(user: Usuario):
     usuarios[user.nombre] = user
     return {"mensaje": f"Usuario {user.nombre} guardado exitosamente"}
+
+
+@app.get("/usuarios/{nombre}")
+def obtener_usuario(nombre: str):
+    if nombre in usuarios:
+        return usuarios[nombre]
+    return {"error": "Usuario no encontrado"}
